79420ddf7f2e5ae19ccfe5134c1e33c7c6580d30
LDG-BBgod <padzz321@naver.com>
2023-11-26 17:52:21 +0900

    test

--- src/routers/apis.js
+++ src/routers/apis.js
@@ -8,5 +8,8 @@ const router = express.Router()
 router.post('/pwCheck', async (req, res) => {
   req.body.value === '4989' ? res.send(true) : res.send(false)
 })
+router.post('/pwCheckCS', async (req, res) => {
+  req.body.value === '7530' ? res.send(true) : res.send(false)
+})
 
 module.exports = router
